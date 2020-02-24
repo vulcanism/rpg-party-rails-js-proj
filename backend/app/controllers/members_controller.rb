f0981e7749b3c53005768fee3875d4931551a80f
Savannah C <cynomania@gmail.com>
2020-02-24 15:40:02 -0500

    member crud setup + params

--- backend/app/controllers/members_controller.rb
+++ backend/app/controllers/members_controller.rb
@@ -1,2 +1,23 @@
 class MembersController < ApplicationController
+    def index
+        members = Member.all
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def member_params
+        params.require(:member).permit(:name, :level, :gender, :race, :role, :party_id)
+    end
 end
