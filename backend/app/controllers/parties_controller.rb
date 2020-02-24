868b9850621c56b67e27dc3ff5bdedbcd84cd1fc
Savannah C <cynomania@gmail.com>
2020-02-24 15:35:23 -0500

    stubbed out party crud, params

--- backend/app/controllers/parties_controller.rb
+++ backend/app/controllers/parties_controller.rb
@@ -1,2 +1,25 @@
 class PartiesController < ApplicationController
+    def index
+        parties = Party.all
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+        party.destroy
+    end
+
+    private
+
+    def party_params
+        params.require(:party).permit(:name, :quest, :color)
+    end
+
 end
