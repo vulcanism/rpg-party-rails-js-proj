a21da8e48f177a8194f94820b49f4f668979dd64
Savannah C <cynomania@gmail.com>
2020-02-25 15:16:31 -0500

    added js constructor methods

---
+++ frontend/src/Member.js
@@ -0,0 +1,11 @@
+class Member {
+    constructor(id, name, level, gender, race, role, partyId) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.gender = gender;
+        this.race = race;
+        this.role = role;
+        this.party_id = partyId;
+    }
+}
---
+++ frontend/src/Party.js
@@ -0,0 +1,8 @@
+class Party {
+    constructor(id, name, quest, color) {
+        this.id = id;
+        this.name = name;
+        this.quest = quest;
+        this.color = color;
+    }
+}
