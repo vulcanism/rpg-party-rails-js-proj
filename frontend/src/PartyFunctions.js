b9dd9fb7897a3e4239799ae0b462145148b76c99
Savannah C <cynomania@gmail.com>
2020-02-26 14:58:54 -0500

    fetch and render parties

--- frontend/src/PartyFunctions.js
+++ frontend/src/PartyFunctions.js
@@ -2,5 +2,13 @@ let allParties = [];
 let allMembers = [];
 
 function fetchParties() {
+    return fetch(PARTIES_URL)
+    .then(response => response.json())
+    .then(json => {
+        renderParties(json)
+    })
+}
+
+function renderParties(json) {
     
 }
