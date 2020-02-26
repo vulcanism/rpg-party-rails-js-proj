a274d785042bd2f02b559432f10a74509a246151
Savannah C <cynomania@gmail.com>
2020-02-26 14:43:22 -0500

    building out create party form

---
+++ frontend/src/PartyForm.js
@@ -0,0 +1,8 @@
+const generatePartyForm = () => {
+    const form = document.createElement("form")
+    const formSubmit = document.createElement("button")
+    formSubmit.textContent = "Create Party"
+
+    form.id = "create-party"
+    formSubmit.id = "form-submit"
+}
