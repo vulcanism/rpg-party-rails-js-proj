132ff25e862038a78bb0f88869fe83ffe1947f32
Savannah C <cynomania@gmail.com>
2020-02-26 15:12:34 -0500

    working on functions

--- frontend/src/PartyFunctions.js
+++ frontend/src/PartyFunctions.js
@@ -10,5 +10,28 @@ function fetchParties() {
 }
 
 function renderParties(json) {
-    
+    json.data.forEach(party => {
+        let newParty = new Party(party.attributes)
+
+        let members = json.included.filter(member => {
+            return member.attributes.party_id == newParty.id
+        })
+
+        createMembers(newParty, members)
+
+        addParty(newParty)
+    })
+
+}
+
+function addParty(party) {
+    const h2 = document.createElement("h2");
+    const h4 = document.createElement("h4");
+    const a = document.createElement("a");
+    a.setAttribute("href", "#")
+    a.innerHTML = `${party.name}`
+
+    a.addEventListener("click", function(e) {
+        partiesContainer.textContet = "";
+    })
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,3 +2,5 @@ const BASE_URL = "http://localhost:3000"
 const PARTIES_URL = `${BASE_URL}/parties`
 const MEMBERS_URL = `${BASE_URL}/members`
 
+let partiesContainer = document.querySelector("div.parties-container")
+let membersContainer = document.querySelector("div.members-container")
