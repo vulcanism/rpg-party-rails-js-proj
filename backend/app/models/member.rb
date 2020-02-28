b0bb3265137bd95f3d600b760de76dad79470250
Savannah C <cynomania@gmail.com>
2020-02-28 11:54:49 -0500

    added validations

--- backend/app/models/member.rb
+++ backend/app/models/member.rb
@@ -1,3 +1,11 @@
 class Member < ApplicationRecord
     belongs_to :party
+
+    ## Name, Level, Gender, Race, Role
+
+    validates :name, presence: true
+    validates :level, presence: true
+    validates :gender, presence: true
+    validates :race, presence: true
+    validates :role, presence: true
 end
--- backend/app/models/party.rb
+++ backend/app/models/party.rb
@@ -1,3 +1,7 @@
 class Party < ApplicationRecord
     has_many :members
+
+    validates :name, presence: true
+    validates :quest, presence: true
+    validates :color, presence: true
 end
